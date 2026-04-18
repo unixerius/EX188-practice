fa2175ce42e4589334dbb03651762042e8471872
Tess Sluijter-Stek <tess@unixerius.nl>
2026-04-18 22:47:09 +0200

    Fix to date command in task 15.

--- playbooks/files/peapod/pop.sh
+++ playbooks/files/peapod/pop.sh
@@ -3,6 +3,6 @@
 while true
 do
 
-  sed "s/^This.*/This page was last updated at: $(date +%H:%M:%s)./" -i /usr/local/apache2/htdocs/index.html
+  sed "s/^This.*/This page was last updated at: $(date +%H:%M:%S)./" -i /usr/local/apache2/htdocs/index.html
 
 done
